312ac0a229e427193ed86c1cbb941f31c19a2f92
U_Know_69 <149695925+UKnow-69@users.noreply.github.com>
2025-09-06 11:52:45 -0500

    Add files via upload

---
+++ gui/dx11/dx11.h
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace GUI {
+namespace DX11 {
+
+// --- Initialization ------------------------------------------------
+
+bool Init();
+void Destroy();
+
+} // namespace DX11
+} // namespace GUI
