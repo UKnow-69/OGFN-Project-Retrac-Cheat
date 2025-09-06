392aaa32f4a96f8069741094d6e5b260d8ba47e8
U_Know_69 <149695925+UKnow-69@users.noreply.github.com>
2025-09-06 11:58:22 -0500

    Delete utils directory

--- utils/error.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <string>
-
-namespace Error {
-
-[[noreturn]] void ThrowError(const std::string& Msg);
-
-} // namespace Error
--- utils/string.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-
-namespace String {
-
-std::wstring NarrowToWide(const std::string& String);
-std::string  WideToNarrow(const std::wstring& WideString);
-
-} // namespace String
